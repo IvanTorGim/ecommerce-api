3e51aca8fe1ffcf37135bc9bab89c860246358ad
Ivan Tortosa Gimeno <itortosagimeno@gmail.com>
2024-10-25 12:52:58 +0200

    product category case insensitive

--- src/main/java/com/itortosagimeno/ecommerce_api/models/dtos/ProductDTO.java
+++ src/main/java/com/itortosagimeno/ecommerce_api/models/dtos/ProductDTO.java
@@ -1,17 +1,20 @@
 package com.itortosagimeno.ecommerce_api.models.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.itortosagimeno.ecommerce_api.models.enums.Category;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.PositiveOrZero;
 
+import static com.fasterxml.jackson.annotation.JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES;
+
 public record ProductDTO(
         @Null Integer id,
         @NotNull @NotBlank String name,
         @NotNull String description,
         @NotNull @PositiveOrZero Double price,
-        @NotNull Category category,
+        @NotNull @JsonFormat(with = ACCEPT_CASE_INSENSITIVE_PROPERTIES) Category category,
         @NotNull String image,
         @NotNull @PositiveOrZero Integer stock
 ) {
