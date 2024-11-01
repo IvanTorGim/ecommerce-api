82b4c66918907661b6dfbd8bf7b39251d53ca2fd
Ivan Tortosa Gimeno <itortosagimeno@gmail.com>
2024-11-01 12:47:36 +0100

    add exceptions

--- src/main/java/com/itortosagimeno/ecommerce_api/product/service/OrderServiceImpl.java
+++ src/main/java/com/itortosagimeno/ecommerce_api/product/service/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.itortosagimeno.ecommerce_api.product.service;
 
 import com.itortosagimeno.ecommerce_api.exception.custom.AddressNotFoundException;
 import com.itortosagimeno.ecommerce_api.exception.custom.OrderNotFoundException;
+import com.itortosagimeno.ecommerce_api.exception.custom.UserNotFoundException;
 import com.itortosagimeno.ecommerce_api.product.model.dto.OrderRequest;
 import com.itortosagimeno.ecommerce_api.product.model.dto.OrderResponse;
 import com.itortosagimeno.ecommerce_api.product.model.mapper.OrderMapper;
@@ -9,6 +10,7 @@ import com.itortosagimeno.ecommerce_api.product.model.mapper.OrderProductMapper;
 import com.itortosagimeno.ecommerce_api.product.repository.OrderProductRepository;
 import com.itortosagimeno.ecommerce_api.product.repository.OrderRepository;
 import com.itortosagimeno.ecommerce_api.user.repository.AddressRepository;
+import com.itortosagimeno.ecommerce_api.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,11 +22,18 @@ public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final OrderProductRepository orderProductRepository;
     private final AddressRepository addressRepository;
+    private final UserRepository userRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository, OrderProductRepository orderProductRepository, AddressRepository addressRepository) {
+    public OrderServiceImpl(
+            OrderRepository orderRepository,
+            OrderProductRepository orderProductRepository,
+            AddressRepository addressRepository,
+            UserRepository userRepository
+    ) {
         this.orderRepository = orderRepository;
         this.orderProductRepository = orderProductRepository;
         this.addressRepository = addressRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -37,6 +46,8 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<OrderResponse> getOrdersByAddressId(final Integer addressId) {
+        final var exists = addressRepository.existsById(addressId);
+        if (!exists) throw new AddressNotFoundException(addressId);
         return orderRepository.findAllByAddressId(addressId)
                 .stream()
                 .map(OrderMapper::toResponse)
@@ -45,6 +56,8 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<OrderResponse> getOrdersByUserId(final Integer userId) {
+        final var exists = userRepository.existsById(userId);
+        if (!exists) throw new UserNotFoundException(userId);
         return orderRepository.findAllByUserId(userId)
                 .stream()
                 .map(OrderMapper::toResponse)
@@ -53,6 +66,8 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderResponse getOrderById(final Integer id) {
+        final var exists = orderRepository.existsById(id);
+        if (!exists) throw new OrderNotFoundException(id);
         return orderRepository.findById(id)
                 .map(OrderMapper::toResponse)
                 .orElseThrow(() -> new OrderNotFoundException(id));
